8c614914decd547e695f95c053f4552bf64d3976
Sai Koka <ssk177@scarletmail.rutgers.edu>
2020-06-01 14:14:45 -0400

    Added error highlighting and fixed prompt text format

--- typing_test.js
+++ typing_test.js
@@ -1,8 +1,19 @@
 var promptText=document.querySelector("#prompt1").innerHTML;
 var count=60;
 var text = document.querySelector("#textbox");
+//var actualText = document.querySelector("")
 
 text.addEventListener("keypress",timerStart,{once : true});
+text.addEventListener("keyup", checkText, false);
+
+function checkText(){
+    if(text.value!==promptText.substring(0,text.value.length)){
+        text.style.color="red";
+    }
+    else{
+        text.style.color="black";
+    }
+}
 
 function timerStart(){
     setInterval(decCount, 1000);
